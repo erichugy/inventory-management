8e18255292dab5152b952100f8a36ca8b3acefc1
EricHugyLIDD <eric.huang@lidd.com>
2024-07-19 01:04:53 -0400

    BootStrap: Horizontal columns and centered button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,43 +11,61 @@ export default function SearchBar(props) {
 	};
 
 	return (
-		<div>
-			<h2>Search for an Item</h2>
-			<form>
-				<label htmlFor="name-field">Name:</label>
-				<input
-					type="text"
-					id="name-field"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-				/>
-				<label htmlFor="price-field">Max Price</label>
-				<input
-					type="number"
-					id="price-field"
-					value={price}
-					onChange={(e) => setPrice(e.target.value)}
-				/>
-				<label htmlFor="type-field">Type:</label>
-				<input
-					type="text"
-					id="type-field"
-					value={type}
-					onChange={(e) => setType(e.target.value)}
-				/>
-				<label htmlFor="brand-field">Brand:</label>
-				<input
-					type="text"
-					id="brand-field"
-					value={brand}
-					onChange={(e) => setBrand(e.target.value)}
-				/>
+		<div className="container">
+			<div className="row">
+				<h2>Search for an Item</h2>
+			</div>
+			<div className="row">
+				<div className="col">
+					<label htmlFor="name-field">Name:</label>
+					<input
+						type="text"
+						id="name-field"
+						className="form-control"
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
+				</div>
+				<div className="col">
+					<label htmlFor="price-field">Max Price:</label>
+					<input
+						type="number"
+						id="price-field"
+						className="form-control"
+						value={price}
+						onChange={(e) => setPrice(e.target.value)}
+					/>
+				</div>
+				<div className="col">
+					<label htmlFor="type-field">Type:</label>
+					<input
+						type="text"
+						id="type-field"
+						className="form-control"
+						value={type}
+						onChange={(e) => setType(e.target.value)}
+					/>
+				</div>
+				<div className="col">
+					<label htmlFor="brand-field">Brand:</label>
+					<input
+						type="text"
+						id="brand-field"
+						className="form-control"
+						value={brand}
+						onChange={(e) => setBrand(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className="row mt-3">
+				<div className="col-4" />
 				<button
 					type="button"
+					className="col-4 btn btn-primary"
 					onClick={searchButtonPressed}>
 					Search
 				</button>
-			</form>
+			</div>
 		</div>
 	);
 }
