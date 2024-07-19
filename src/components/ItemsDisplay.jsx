1a5a56301f63160ad46d1089fc3652648c4fd624
EricHugyLIDD <eric.huang@lidd.com>
2024-07-19 01:34:17 -0400

    Bootstrap: Styled Tables

--- src/components/ItemsDisplay.jsx
+++ src/components/ItemsDisplay.jsx
@@ -1,23 +1,38 @@
-import { useState } from "react";
-import styles from "../styles/itemsDisplay.module.css";
 
 export default function ItemsDisplay(props) {
 	const showItem = (item) => {
 		return (
-			<div key={item.id}>
-				<p style={{
-					color: "red",
-					fontSize: "20px"
-				}}>Id: {item.id}</p>
-				<p>Name: {item.name}</p>
-				<p>Price: {item.price}</p>
-				<p>Type: {item.type}</p>
-				<p>Brand: {item.brand}</p>
-				<p className={styles.blue}>Blue Text</p>
-				<p className={styles.red}>Red Text</p>
-			</div>
+			<tr>
+				<th scope="row">{item.id}</th>
+				<td>{item.name}</td>
+				<td>{item.price}</td>
+				<td>{item.type}</td>
+				<td>{item.brand}</td>
+			</tr>
 		);
 	};
 
-	return <div>{props.items?.map((item) => showItem(item))}</div>;
+	return (
+		<div className="container">
+			<div className="row">
+				<h2>Items</h2>
+			</div>
+			<div className="row">
+				<table className="table table-striped">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">Name</th>
+							<th scope="col">Price</th>
+							<th scope="col">Type</th>
+							<th scope="col">Brand</th>
+						</tr>
+					</thead>
+					<tbody>
+						{props.items?.map((item) => showItem(item))}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
